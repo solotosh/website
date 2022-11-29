6979655a2cd6d35b033d9141205c9b6e033444ee
Solomon Gitonga <unimediaafrica@gmail.com>
2022-11-29 20:13:19 +0300

    Add files via upload

---
+++ wiyeta/about.php
@@ -0,0 +1,93 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>About Us</title>
+    <!-- Bootstrap -->
+	<link href="css/bootstrap-4.4.1.css" rel="stylesheet">
+	<link href="style.css" rel="stylesheet" type="text/css">
+  </head>
+<body>
+    <div class="container">
+<div class="container-fluid">
+  <nav class="navbar navbar-expand-lg navbar-light bg-light"> <a class="navbar-brand" href="index.php"><img src="images/logo.png" alt="" width="85" height="85" class="img-fluid"/></a>
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent1" aria-controls="navbarSupportedContent1" aria-expanded="false" aria-label="Toggle navigation"> <span class="navbar-toggler-icon"></span> </button>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent1">
+            <ul class="navbar-nav mr-auto">
+              <li class="nav-item active"> <a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a> </li>
+              <li class="nav-item"> <a class="nav-link" href="about.php">About</a> </li>
+              <li class="nav-item"> <a class="nav-link" href="download.php">Downloads</a></li>
+              <li class="nav-item"> <a class="nav-link" href="contact.php">Contact</a></li>
+            </ul>
+</div>
+        </nav>
+</div>
+
+<div><img src="images/football.png" width="600" height="400" alt=""/>></div>
+<!-- body code goes here -->
+		
+<h1 class="text-center">About US</h1>
+<div class="row">
+  <div> HISTORY OF THE SCHOOL
+
+    <p class="text-lowercase"><em>The term “ Wiyeta” is a Bukusu word meaning “ Self help”
+      - Wiyeta Girls secondary school is located at Kipsoen scheme 18km from Kitale town on Kitale - Kapenguria road, 4 Kms from Kipsaina centre.
+      
+      It is in Trans-Nzoia County, the former R. Valley province, Trans-Nzoia East district, Sinyerere location. It is on a 11 acre land and 2 ½ acre farm. 
+      The school enjoys a, quiet, cool environment at the foot of Cherangany hills. It borders Saiwa swamp to the West and Kabolet forest to the East.
+      
+      It was started in 1982 as Kipsoen sec school in the Kipsoen scheme in the home of a settler Mr Toftee (currently the Wiyeta dispensary) with an enrolment of 70 students headed by Mr. Kibisu as community school (harambee).
+      
+      It collapsed in 1984 because of the arrest of the school’s founders namely:- Mr Walter Odera, Mr. Kibisu and Rev Muhia because of running a non- registered school.
+      
+      It resumed in 1986 after registration under the headship of Mr Wamutondo the then Headteacher of Wiyeta primary school. The school was transferred to its present parcel of land and the name changed from Kipsoen Seocondary School to Wiyeta mixed secondary school.
+      
+      In 1987 TSC posted a headteacher Mr Mukassa to the school. The school established   the first Board Of Governors in 1987 led by Mr Kanyanga who was succeeded by Mr Opimo upto 2007 with RCEA as the sponsor.
+      
+      The school was under- enrolled hence it was proposed that the girls be provided with boarding facilities to cater for girls coming from far and this was done in 1993. The sponsor built the first dormitory. In 1996 the school was proposed to be a full girls boarding which was effected in 2002.
+      
+      The school population started picking up in 1999 and was finally registered as a double stream girls boarding in 2003.  Mr Mukassa retired in August 2005 and was replaced by Mrs Roseline Illa.
+      
+      The school’s steady rise in enrolment is attributed to its excellence performance in girls football nationally and East and Central Africa.
+      
+      The school has an established functional board chaired by Mr Alfred Mbako and an established PTA chaired by Justus T. Kamakia.
+      
+      Currently the students population is 340 with a teaching staff of 13 TSC teachers, 4 BOG teachers and a support staff of 15 members. The school has 8 classrooms an administration block, two laboratories, 3 dormitories, a kitchen and one staff house.
+      The school is supplied with electricity by KPLC and has a stand- by generator. The school receives its water supply form a nearby dam. It gets its health services from the nearby Wiyeta health centre.
+      
+      The first KCSE candidate took their exams at St Christopher’s secondary school as a centre. The school became an examination centre in 1991 and since then the academic performance has been rising steadily.
+      
+      It also competes favorably in most co-curricular activities. Wiyeta aspires to be a school of choice and excellence in moulding the girl child so that she becomes a successive and responsible role model in the society.</em></p>
+  </div>
+</div>
+  
+  
+</div>
+    </div>
+	  
+</div>
+	  <br>
+ 
+    </div>
+</div>
+	   <footer> </div>
+		   <div class="row">
+	     <div class="col-md-6 offset-md-1">
+	       <div><img src="images/logo.png" alt="" width="100" height="90" class="img-fluid"/></div>
+	     </div>
+	     <div class="col-md-6 offset-md-6">Copy Right All Rights Reserved</div>
+   
+	  </footer> 
+    
+  </div>
+
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) --> 
+<script src="js/jquery-3.4.1.min.js"></script>
+
+	<!-- Include all compiled plugins (below), or include individual files as needed -->
+<script src="js/popper.min.js"></script> 
+<script src="js/bootstrap-4.4.1.js"></script>
+</body>
+</html>
---
+++ wiyeta/index.php
@@ -0,0 +1,138 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Wiyeta School</title>
+    <!-- Bootstrap -->
+	<link href="css/bootstrap-4.4.1.css" rel="stylesheet">
+	<link href="style.css" rel="stylesheet" type="text/css">
+  </head>
+  <body>
+    <div class="container">
+      <div class="container-fluid">
+        <nav class="navbar navbar-expand-lg navbar-light bg-light"> <a class="navbar-brand" href="index.php"><img src="images/logo.png" alt="" width="85" height="85" class="img-fluid"/></a>
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent1" aria-controls="navbarSupportedContent1" aria-expanded="false" aria-label="Toggle navigation"> <span class="navbar-toggler-icon"></span> </button>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent1">
+            <ul class="navbar-nav mr-auto">
+              <li class="nav-item active"> <a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a> </li>
+              <li class="nav-item"> <a class="nav-link" href="about.php">About</a> </li>
+              <li class="nav-item"> <a class="nav-link" href="download.php">Downloads</a></li>
+              <li class="nav-item"> <a class="nav-link" href="contact.php">Contact</a></li>
+            </ul>
+</div>
+        </nav>
+	    <div id="carouselExampleIndicators1" class="carousel slide" data-ride="carousel" style="background-color: grey">
+		    <ol class="carousel-indicators">
+		      <li data-target="#carouselExampleIndicators1" data-slide-to="0" class="active"></li>
+		      <li data-target="#carouselExampleIndicators1" data-slide-to="1"></li>
+		      <li data-target="#carouselExampleIndicators1" data-slide-to="2"></li>
+	      </ol>
+		    <div class="carousel-inner" role="listbox">
+		      <div class="carousel-item active"> <img class="d-block mx-auto img-fluid" src="images/football.png" alt="First slide">
+		        <div class="carousel-caption">
+		          <h5>Football Team In Arusha Tanzania</h5>
+		          <p> </p>
+	            </div>
+	          </div>
+		      <div class="carousel-item"> <img class="d-block mx-auto img-fluid" src="images/football1.png" alt="Second slide">
+		        <div class="carousel-caption">
+		          <h5>Nakuru Nationals</h5>
+		          <p></p>
+	            </div>
+	          </div>
+		      <div class="carousel-item"> <img class="d-block mx-auto img-fluid" src="images/hocker.png" alt="Third slide"><img src="images/hockey.png" width="600" height="400" alt=""/>
+		        <div class="carousel-caption">
+		          <h5>Hockey Team</h5>
+		          <p></p>
+	            </div>
+	          </div>
+	      </div>
+		    <a class="carousel-control-prev" href="#carouselExampleIndicators1" role="button" data-slide="prev"> <span class="carousel-control-prev-icon" aria-hidden="true"></span> <span class="sr-only">Previous</span> </a> <a class="carousel-control-next" href="#carouselExampleIndicators1" role="button" data-slide="next"> <span class="carousel-control-next-icon" aria-hidden="true"></span> <span class="sr-only">Next</span> </a> </div>
+      </div>
+  <hgroup>
+    <h1 class="text-center">SCHOOL MOTTO</h1>
+  </hgroup>
+  <h1 class="text-center">Determination for success</h1>
+  
+		
+  <!-- body code goes here -->
+  <div class="row">
+    <div class="col-md-4">
+      <div class="card col-md-12"> <img class="card-img-top rounded img-fluid" src="images/akinyi.png" alt="Card image cap">
+        <div class="card-body">
+          <h5 class="card-title">Lavender Akinyi &nbsp;</h5>
+          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+          <a href="#" class="btn btn-primary">Go somewhere</a> </div>
+      </div>
+    </div>
+    <div class="col-md-4">
+      <div class="card col-md-12"> <img class="card-img-top rounded img-fluid" src="images/FB_IMG_1622219973840.jpg" alt="Card image cap">
+        <div class="card-body">
+          <h5 class="card-title">Card title</h5>
+          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+          <a href="#" class="btn btn-primary">Go somewhere</a> </div>
+      </div>
+    </div>
+    <div class="col-md-4">
+      <div class="card col-md-12"> <img class="card-img-top img-fluid" src="images/IMG_20201121_130358.jpg" alt="Card image cap">
+        <div class="card-body">
+          <h5 class="card-title">Card title</h5>
+          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+          <a href="#" class="btn btn-primary">Go somewhere</a> </div>
+      </div>
+    </div>
+	  
+  </div>
+	  <br>
+  <div id="accordion1" role="tablist">
+    <div class="card">
+      <div class="card-header" role="tab" id="headingOne1">
+        <h5 class="mb-0"> <a data-toggle="collapse" href="#collapseOne1" role="button" aria-expanded="true" aria-controls="collapseOne1"> Our Mission </a> </h5>
+      </div>
+      <div id="collapseOne1" class="collapse show" role="tabpanel" aria-labelledby="headingOne1" data-parent="#accordion1">
+        <div class="card-body">
+DETERMINATION FOR SUCCESS</div>
+      </div>
+    </div>
+    <div class="card">
+      <div class="card-header" role="tab" id="headingTwo1">
+        <h5 class="mb-0"> <a class="collapsed" data-toggle="collapse" href="#collapseTwo1" role="button" aria-expanded="false" aria-controls="collapseTwo1"> Our Vision&nbsp; &nbsp; </a> </h5>
+      </div>
+      <div id="collapseTwo1" class="collapse" role="tabpanel" aria-labelledby="headingTwo1" data-parent="#accordion1">
+        <div class="card-body">“ AN INSTITUTION OF EXCELLENCE IN EMPOWERING THE GIRL CHILD” </div>
+      </div>
+    </div>
+    <div class="card">
+      <div class="card-header" role="tab" id="headingThree1">
+        <h5 class="mb-0"> <a class="collapsed" data-toggle="collapse" href="#collapseThree1" role="button" aria-expanded="false" aria-controls="collapseThree1"> MISSION </a> </h5>
+      </div>
+      <div id="collapseThree1" class="collapse" role="tabpanel" aria-labelledby="headingThree1" data-parent="#accordion1">
+        <div class="card-body">“TO MOULD AN ALL ROUND 
+STUDENT THROUGH 
+VALUE ADDITION  IN THE 
+PROVISION OF QUALITY AND 
+RELEVANT  EDUCATION”</div>
+      </div>
+		    </div>
+    </div>
+	   <footer> </div>
+		   <div class="row">
+	     <div class="col-md-6 offset-md-1">
+	       <div><img src="images/logo.png" alt="" width="105" height="95" class="img-fluid"/></div>
+	     </div>
+	     <div class="col-md-6 offset-md-6">Copy Right All Rights Reserved</div>
+   
+	  </footer> 
+    
+  </div>
+
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) --> 
+	<script src="js/jquery-3.4.1.min.js"></script>
+
+	<!-- Include all compiled plugins (below), or include individual files as needed -->
+	<script src="js/popper.min.js"></script> 
+  <script src="js/bootstrap-4.4.1.js"></script>
+  </body>
+</html>
